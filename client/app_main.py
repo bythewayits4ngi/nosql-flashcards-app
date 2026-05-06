073785e8fa0788037d4c0960bbcf77e0fe100635
Angelina <holl.angelina@icloud.com>
2026-05-06 10:45:07 +0200

    new client

--- client/app_main.py
+++ client/app_main.py
@@ -2,13 +2,14 @@ import requests
 
 url = "http://127.0.0.1:8000/cards"
 
-def zeige_alle_karten():
-    response = requests.get(url)
-    if response.status_code == 200:
-        karten = response.json()
-        for karte in karten:
-            print(f"Frage: {karte['question']}")
-    else:
-        print("Fehler beim Abrufen der Daten")
+response = requests.get(url)
 
-zeige_alle_karten()
+if response.status_code == 200:
+    cards = response.json()
+    
+    for card in cards:
+        print("Frage:", card["question"])
+        print("Antwort:", card["answer"])
+        print("------")
+else:
+    print("Fehler:", response.status_code)
